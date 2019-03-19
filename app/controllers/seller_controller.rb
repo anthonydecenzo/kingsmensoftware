84b41a815fa09a4df9d3e3526bb3a757a796a950
anthonydecenzo <anthonydecenzo8@gmail.com>
2019-03-19 14:53:42 -0400

    the output for changing a database entry value and updating it does not output anymore

--- app/controllers/seller_controller.rb
+++ app/controllers/seller_controller.rb
@@ -1,5 +1,6 @@
 class SellerController < ApplicationController
   def index
-  	@orders = Order.where("SellerId = ? AND Notified = false", current_user.id)
+  	@orders = Order.where("SellerId = ?", current_user.id)
+  	@ordersNew = Order.where("SellerId = ? AND Notified = false", current_user.id)
   end
 end
