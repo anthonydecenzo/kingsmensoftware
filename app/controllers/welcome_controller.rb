dec36a1f695c3bff63f06e3f3d9098af63200840
anthonydecenzo <anthonydecenzo8@gmail.com>
2019-03-19 00:08:22 -0400

    generated dashboard controller and a redirect to dashboard if user is already logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
+	before_action :check_signed_in
+
 	def index
 
+	end
+
+	def check_signed_in
+		redirect_to dashboard_index_path if signed_in?
 	end
 end
