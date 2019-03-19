212b19877e1ed126f0f59609a436221ac12b9a5d
anthonydecenzo <anthonydecenzo8@gmail.com>
2019-03-19 00:35:20 -0400

    added side navbar for dashboard index. also added a check to see if user is signed in when visiting dashboard URL

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class DashboardController < ApplicationController
+
+	before_action :check_signed_in
+
   def index
+  end
+
+  def check_signed_in
+  	redirect_to user_session_path if !signed_in?
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
+	
 	before_action :check_signed_in
-
 	def index
-
 	end
+
 
 	def check_signed_in
 		redirect_to dashboard_index_path if signed_in?
